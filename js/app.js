c2ac8d825ff652861b6653e1fcbf33088ec7990c
eljut <eju@kth.se>
2015-02-11 17:19:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,5 +5,6 @@ $(function() {
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
 	var screenAfterView = new ScreenAfterView($("#screenAfterView"), model);
+	var selectedDishView = new SelectedDishView($("#selectedDishView"), model);
 
 });
