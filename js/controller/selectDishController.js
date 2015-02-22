5f017743989f56fa120ea401b70be03fa8a720cb
apan1000 <fredrik.911225@gmail.com>
2015-02-22 23:15:06 +0100

    Search function added to selectDishController/view

--- js/controller/selectDishController.js
+++ js/controller/selectDishController.js
@@ -3,14 +3,48 @@ var SelectDishController = function (view,model,stateController) {
 
 	//http://learn.jquery.com/events/event-delegation/
 
-	//Change Dishes
+	//Change Dishes by type
 	view.container.on("change", "#dish-type", function(event) {
 		console.log("---Changing dishes by dish type!");
-		console.log("Value: " + $(this).val() + "!");
-		var allDishes = model.getAllDishes($(this).val());
-		$("#dishes").html("");
+		console.log("Value: " + this.value + "!");
+		showDishes(this.value);
+	});
+
+	//Search when search-button is clicked
+	view.container.on("click", "#search-button", function(event) {
+		var keyWords = view.container.find("#search-bar").val();
+		var type = view.container.find("#dish-type").val();
+
+		console.log("---Searching with key words: "+keyWords+" and type: "+type+"\n");
+
+		showDishes(type,keyWords);
+	});
+
+	//Search when enter is pressed in search-bar
+	view.container.on("keydown", "#search-bar", function(event) {
+		if(event.which == 13) {
+			var keyWords = view.container.find("#search-bar").val();
+			var type = view.container.find("#dish-type").val();
+
+			console.log("---Searching with key words: "+keyWords+" and type: "+type+"\n");
+
+			showDishes(type,keyWords);
+		}
+	});
+
+	//Show the clicked dish
+	view.container.on("click", ".dish-head", function(event) {
+		var id = $(this).parent().data("dish-id");
+		stateController.showSelectedDish(id);
+	});
+
+	//Shows all dishes of stated type. Can be filtered.
+	var showDishes = function(type,filter) {
+		var allDishes = model.getAllDishes(type,filter);
+		var dishes = view.container.find("#dishes");
+		dishes.html("");
 		for ( var i = 0; i < allDishes.length; i++ ) {
-			$("#dishes").append(
+			dishes.append(
 				'<div class="dish" data-dish-id="'+allDishes[i].id+'">'+
 					'<div class="dish-head">'+
 						'<img src="images/'+allDishes[i].image+'" alt="'+allDishes[i].name+'">'+
@@ -20,12 +54,6 @@ var SelectDishController = function (view,model,stateController) {
 				'</div>'
 				);
 		}
-	});
+	}
 
-	view.container.on("click", ".dish-head", function(event) {
-		var id = $(this).parent().data("dish-id");
-		console.log("Clicked dish with id "+id);
-		//model.addDishToMenu(id);
-		stateController.showSelectedDish(id);
-	});
 }
