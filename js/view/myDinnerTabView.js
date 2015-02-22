3403da03a85b657909dd81719f15fd56660a62cf
apan1000 <fredrik.911225@gmail.com>
2015-02-22 15:47:03 +0100

    Small update to myDinnerTabView
    Menu stays sorted starter-main-dessert

--- js/view/myDinnerTabView.js
+++ js/view/myDinnerTabView.js
@@ -13,63 +13,80 @@ var MyDinnerTabView = function (container,model) {
 		this.container.addClass("col-sm-3 full-height");
 
 		this.container.append(
-					'<h3>My Dinner</h3>'+
-					'<div>'+
-						'<label for="number-of-guests">People</label>'+
-						'<input type="number" step="1" name="People" id="number-of-guests" value="'+model.getNumberOfGuests()+'">'+
-					'</div>'+
-					'<div id="menu-head" class="row th">'+
-						'<div class="col-sm-3 dinner-col">'+
-							'Dish'+
-						'</div>'+
-						'<div class="col-sm-6 dinner-col">'+
-							'Name'+
-						'</div>'+
-						'<div class="col-sm-3 dinner-col">'+
-							'Cost'+
-						'</div>'+
-					'</div>'+
-					'<div id="menu"></div>'
+			'<h3>My Dinner</h3>'+
+			'<div>'+
+				'<label for="number-of-guests">People</label>'+
+				'<input type="number" step="1" name="People" id="number-of-guests" value="'+model.getNumberOfGuests()+'">'+
+			'</div>'+
+			'<div id="menu-head" class="row th">'+
+				'<div class="col-sm-3 dinner-col">'+
+					'Dish'+
+				'</div>'+
+				'<div class="col-sm-6 dinner-col">'+
+					'Name'+
+				'</div>'+
+				'<div class="col-sm-3 dinner-col">'+
+					'Cost'+
+				'</div>'+
+			'</div>'+
+			'<div id="menu">'+
+				'<div id="menu-starter"></div>'+
+				'<div id="menu-main"></div>'+
+				'<div id="menu-dessert"></div>'+
+			'</div>'
 		);
 
 		// Add dishes to menu
 		var menu = model.getFullMenu();
+		var menuItemContent = '';
 		for ( var i = 0; i < menu.length; i++ ) {
-			$("#menu").append(
-					'<div class="row menu-item">'+
-						'<a href="#remove" title="Remove" class="remove">X</a>'+
-						'<div class="col-sm-3 dinner-col">'+
-							menu[i].id+
-						'</div>'+
-						'<div class="col-sm-6 dinner-col">'+
-							menu[i].name+
-						'</div>'+
-						'<div class="col-sm-3 dinner-col">'+
-							model.getDishPrice(menu[i].id)+
-						'</div>'+
-					'</div>'
-			);
+			menuItemContent = 
+				'<a href="#remove" title="Remove" class="remove">X</a>'+
+				'<div class="col-sm-3 dinner-col">'+
+					menu[i].id+
+				'</div>'+
+				'<div class="col-sm-6 dinner-col">'+
+					menu[i].name+
+				'</div>'+
+				'<div class="col-sm-3 dinner-col">'+
+					model.getDishPrice(menu[i].id)+
+				'</div>'
+			switch(menu[i].type) {
+				case "starter":
+					$("#menu-starter").addClass("row menu-item");
+					$("#menu-starter").html(menuItemContent);
+					break;
+				case "main dish":
+					$("#menu-main").addClass("row menu-item");
+					$("#menu-main").html(menuItemContent);
+					break;
+				case "dessert":
+					$("#menu-dessert").addClass("row menu-item");
+					$("#menu-dessert").html(menuItemContent);
+					break;
+			}
+			
 		}
 
 		this.container.append(
-					'<div class="row">'+
-						'<div class="col-sm-3 dinner-col">'+
-						'</div>'+
-						'<div class="col-sm-6 dinner-col">'+
-							'Pending'+
-						'</div>'+
-						'<div class="col-sm-3 dinner-col">'+
-							'0.00'+
-						'</div>'+
-					'</div>'+
-					'<hr>'+
-					'<div class="right-aligned">'+
-						'SEK <span id="total-cost">'+model.getTotalMenuPrice()+'</span>'+
-					'</div>'+
-					'<div class="confirm-div">'+
-						'<button id="confirm-dinner-btn" class="btn btn-default btn-lg" type="button" disabled>Confirm dinner</button>'+
-					'</div>'+
-					'</div>'
+			'<div id="pending" class="row">'+
+				'<div class="col-sm-3 dinner-col">'+
+				'</div>'+
+				'<div class="col-sm-6 dinner-col">'+
+					'Pending'+
+				'</div>'+
+				'<div class="col-sm-3 dinner-col">'+
+					'0.00'+
+				'</div>'+
+			'</div>'+
+			'<hr>'+
+			'<div class="right-aligned">'+
+				'SEK <span id="total-cost">'+model.getTotalMenuPrice()+'</span>'+
+			'</div>'+
+			'<div class="confirm-div">'+
+				'<button id="confirm-dinner-btn" class="btn btn-default btn-lg" type="button" disabled>Confirm dinner</button>'+
+			'</div>'+
+			'</div>'
 		);
 
 		this.numberOfGuests = container.find("#number-of-guests");
@@ -84,25 +101,39 @@ var MyDinnerTabView = function (container,model) {
 	this.update = function(obj) {
 		if (obj === "menuDishAdded") {
 			// Remove dishes from menu
-			container.find("#menu").children().remove();
+			container.find("#menu-starter").empty();
+			container.find("#menu-main").empty();
+			container.find("#menu-dessert").empty();
 			
 			// Add dishes to menu
 			var menu = model.getFullMenu();
+			var menuItemContent = '';
 			for ( var i = 0; i < menu.length; i++ ) {
-				$("#menu").append(
-					'<div class="row menu-item">'+
-						'<a href="#remove" title="Remove" class="remove">X</a>'+
-						'<div class="col-sm-3 dinner-col">'+
-							menu[i].id+
-						'</div>'+
-						'<div class="col-sm-6 dinner-col">'+
-							menu[i].name+
-						'</div>'+
-						'<div class="col-sm-3 dinner-col">'+
-							model.getDishPrice(menu[i].id)+
-						'</div>'+
+				menuItemContent = 
+					'<a href="#remove" title="Remove" class="remove">X</a>'+
+					'<div class="col-sm-3 dinner-col">'+
+						menu[i].id+
+					'</div>'+
+					'<div class="col-sm-6 dinner-col">'+
+						menu[i].name+
+					'</div>'+
+					'<div class="col-sm-3 dinner-col">'+
+						model.getDishPrice(menu[i].id)+
 					'</div>'
-				);
+				switch(menu[i].type) {
+					case "starter":
+						$("#menu-starter").addClass("row menu-item");
+						$("#menu-starter").html(menuItemContent);
+						break;
+					case "main dish":
+						$("#menu-main").addClass("row menu-item");
+						$("#menu-main").html(menuItemContent);
+						break;
+					case "dessert":
+						$("#menu-dessert").addClass("row menu-item");
+						$("#menu-dessert").html(menuItemContent);
+						break;
+				}
 			}
 
 			$("#total-cost").html(model.getTotalMenuPrice());
