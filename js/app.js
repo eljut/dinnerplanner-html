bd293deaafa2badfcd88a4d6903d36e0015d949a
apan1000 <fredrik.911225@gmail.com>
2015-02-26 00:05:00 +0100

    Added documents with BigOven data structures

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
-	//Add dishes for testing
-	/*model.addDishToMenu(1);
-	model.addDishToMenu(100);
-	model.addDishToMenu(200);
-	model.addDishToMenu(2);*/
 	
 	//And create the needed controllers and views
 	var navbarView = new NavbarView($("#navbar"), model);
